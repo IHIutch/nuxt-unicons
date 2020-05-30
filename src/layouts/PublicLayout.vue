aefd535c7aa43cf4745c08f1e79e5373d657cfc1
IHIutch <jbhutch01@gmail.com>
2020-05-29 21:49:52 -0400

    Copy the rest of the files

---
+++ src/layouts/PublicLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-navbar type="light" variant="white" fixed="top" class="shadow-sm h-14">
+      <b-navbar-brand to="/">
+        Buffalo 311
+      </b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-button variant="primary" to="/create">
+          Create New Report
+        </b-button>
+      </b-navbar-nav>
+    </b-navbar>
+    <div class="pt-14">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PublicLayout"
+};
+</script>
